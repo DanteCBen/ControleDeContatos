f55672cf52b4dc37870cb8bbc49dfe84ffa70e18
Dante Benicio <dantecosta79@gmail.com>
2024-02-23 11:06:00 -0300

    repository atualizado

--- ControleDeContatos/Repository/ContatoRepository.cs
+++ ControleDeContatos/Repository/ContatoRepository.cs
@@ -17,4 +17,46 @@ public class ContatoRepository : IContatoRepository
 
         return contato;
     }
+
+    public bool Apagar(int id)
+    {
+        var contato = ListarPorId(id);
+
+        if (contato == null) throw new Exception("Erro ao apagar o contato.");
+
+        _bancoContext.Remove(contato);
+        _bancoContext.SaveChanges();
+
+        return true;
+    }
+
+    public ContatoModel Atualizar(ContatoModel contato)
+    {
+        var contatoBuscado = ListarPorId(contato.Id);
+
+        if (contatoBuscado == null) throw new Exception("Houve um erro na atualização do contato.");
+
+        contatoBuscado.Nome = contato.Nome;
+        contatoBuscado.Email = contato.Email;
+        contatoBuscado.Celular = contato.Celular;
+
+        _bancoContext.Contatos.Update(contatoBuscado);
+        _bancoContext.SaveChanges();
+
+        return contatoBuscado;
+    }
+
+    public List<ContatoModel> BuscarTodos()
+    {
+        var allContatos = _bancoContext.Contatos.ToList();
+
+        return allContatos;
+    }
+
+    public ContatoModel ListarPorId(int id)
+    {
+        var contato = _bancoContext.Contatos.FirstOrDefault(contato => contato.Id == id);
+
+        return contato!;
+    }
 }
--- ControleDeContatos/Repository/IContatoRepository.cs
+++ ControleDeContatos/Repository/IContatoRepository.cs
@@ -5,5 +5,9 @@ namespace ControleDeContatos.Repository;
 public interface IContatoRepository
 {
     ContatoModel Adicionar(ContatoModel contato);
+    ContatoModel ListarPorId(int id);
+    ContatoModel Atualizar(ContatoModel contato);
+    bool Apagar(int id);
+    List<ContatoModel> BuscarTodos();
 
 }
