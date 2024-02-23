1a430decf7709630c8204362a3f16c5ebbd071cb
Dante Benicio <dantecosta79@gmail.com>
2024-02-23 11:06:31 -0300

    adicionado novas features

--- ControleDeContatos/Controllers/ContatoController.cs
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ControleDeContatos.Models;
+using ControleDeContatos.Repository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ControleDeContatos.Controllers;
 public class ContatoController : Controller
 {
+    private readonly IContatoRepository _contatoRepository;
+    public ContatoController(IContatoRepository contatoRepository)
+    {
+        _contatoRepository = contatoRepository;
+    }
     public IActionResult Index()
     {
-        return View();
+        var contatos = _contatoRepository.BuscarTodos();
+
+        return View(contatos);
     }
 
     public IActionResult Criar()
@@ -13,13 +22,41 @@ public class ContatoController : Controller
         return View();
     }
 
-    public IActionResult Editar()
+    public IActionResult Editar(int id)
     {
-        return View();
+        var contato = _contatoRepository.ListarPorId(id);
+
+        return View(contato);
     }
 
-    public IActionResult ApagarConfirmacao()
+    public IActionResult ApagarConfirmacao(int id)
     {
-        return View();
+        var contato = _contatoRepository.ListarPorId(id);
+
+        return View(contato);
+    }
+
+    [HttpPost]
+    public IActionResult Apagar(int id)
+    {
+        _contatoRepository.Apagar(id)
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult Criar(ContatoModel contato)
+    {
+        _contatoRepository.Adicionar(contato);
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public IActionResult Atualizar(ContatoModel contato)
+    {
+        _contatoRepository.Atualizar(contato);
+
+        return RedirectToAction("Index");
     }
 }
