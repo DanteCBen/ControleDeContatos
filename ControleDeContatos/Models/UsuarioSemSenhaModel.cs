69816353f6ee4ef0ccad28b2bf2595596b453cc3
Dante Benicio <dantecosta79@gmail.com>
2024-02-28 23:25:12 -0300

    criado a model usuariosemsenha

---
+++ ControleDeContatos/Models/UsuarioSemSenhaModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using ControleDeContatos.Enums;
+
+namespace ControleDeContatos.Models;
+
+public class UsuarioSemSenhaModel
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Digite o nome do usuário.")]
+    public required string Nome { get; set; }
+
+    [Required(ErrorMessage = "Digite o login do usuário.")]
+    public required string Login { get; set; }
+
+    [Required(ErrorMessage = "Digite o email do usuário.")]
+    [EmailAddress(ErrorMessage = "O e-mail informado não é válido!")]
+    public required string Email { get; set; }
+    [Required(ErrorMessage = "Informe o perfil do usuário.")]
+    public PerfilEnum? Perfil { get; set; }
+
+}
